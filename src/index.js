2c8967989160911b01e3a1f9fd870fd11a8a670f
lxieyang <lxieyang@umich.edu>
2022-10-05 10:51:23 -0400

    fix deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'; // https://stackoverflow.com/a/71985764
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
